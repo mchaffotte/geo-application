9cd5ac46ad48dded4b9087b530ecf32c7aaa02f6
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2018-03-06 22:59:30 +0100

    import shared module instead of translate module

--- geo-front/src/app/admin/admin.module.ts
+++ geo-front/src/app/admin/admin.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule } from '@ngx-translate/core';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { CountriesComponent } from './countries/countries.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SandboxComponent } from './sandbox/sandbox.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     NgbModule,
     CommonModule,
     AdminRoutingModule,
-    TranslateModule
+    SharedModule
   ],
   declarations: [
     AdminComponent,
