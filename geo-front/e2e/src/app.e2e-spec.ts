32b48ec337414f21da0a0ff0d9733ae6ae25795d
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2018-11-29 23:31:45 +0100

    migrate to angular 7.1

--- geo-front/e2e/src/app.e2e-spec.ts
+++ geo-front/e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('geo-front App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('GeoAdmin');
+    expect(page.getTitleText()).toEqual('GeoAdmin');
   });
 });
