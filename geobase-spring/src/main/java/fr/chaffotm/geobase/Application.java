724d547d5806cb667963c173cd99208fd00d57e5
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2019-03-19 22:36:59 +0100

    Reduce components, entities scan

---
+++ geobase-spring/src/main/java/fr/chaffotm/geobase/Application.java
@@ -0,0 +1,23 @@
+package fr.chaffotm.geobase;
+
+import fr.chaffotm.geobase.repository.CountryRepository;
+import fr.chaffotm.geobase.service.CountryService;
+import fr.chaffotm.geobase.web.exception.ExceptionHandlingController;
+import fr.chaffotm.geobase.web.rest.CountryRestController;
+import fr.chaffotm.geodata.entity.CountryEntity;
+import fr.chaffotm.geoquiz.entity.QuizEntity;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication(scanBasePackageClasses = {
+        CountryService.class, CountryRestController.class, ExceptionHandlingController.class, CountryRepository.class
+})
+@EntityScan(basePackageClasses = {CountryEntity.class, QuizEntity.class})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
