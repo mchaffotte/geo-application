dfd39d6d5d69809a073eba24a352459ccfdddbb6
Matthieu Chaffotte <matthieu.chaffotte@bonitasoft.com>
2019-09-23 22:42:09 +0200

    Install material ui

--- geoquiz-react/src/App.js
+++ geoquiz-react/src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const App = () => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const SimpleAppBar = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>GeoAdmin</p>
-      </header>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            GeoAdmin
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
 
-export default App;
+export default SimpleAppBar;
