d11ef66d4b29825f262179e9c59995ab8f1e763a
Matthieu Chaffotte <matthieu.chaffotte@gmail.com>
2019-11-22 23:38:01 +0100

    Add cypress

---
+++ geoquiz-react/cypress/integration/dashboard.spec.js
@@ -0,0 +1,8 @@
+describe('Dashboard page Tests', function() {
+  it('should display the application title', () => {
+    cy.visit('/');
+    cy.get('h6')
+      .contains('GeoAdmin')
+      .should('be.visible');
+  });
+});
